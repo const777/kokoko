ae7f449934d0ed537565c87eaa40aa0d0c81b62f
const777 <limitinf1@gmail.com>
2017-11-23 11:47:23 +0200

    add new code to script file

--- script.js
+++ script.js
@@ -8,3 +8,14 @@ class User {
 		return `${this.name} and ${this.age} years old`;
 	}
 }
+
+class Profile extends User {
+	constructor(name, age, sex) {
+		super(name, age);
+		this.sex = sex;
+	}
+
+	showMe() {
+		return super.showMe() + ' and ' + this.sex;
+	}
+}
